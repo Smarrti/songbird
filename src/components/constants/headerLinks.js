54806e4bee6d96981907abb2d5a132e960771d3d
Sergey Bashkin <bashkin2013@mail.ru>
2020-07-30 21:10:31 +0400

    feat: added array of header links

---
+++ src/components/constants/headerLinks.js
@@ -0,0 +1,19 @@
+export default [{
+  name: 'Разминка',
+  classNames: ['headerLink', 'headerLinkActive'],
+}, {
+  name: 'Воробьиные',
+  classNames: ['headerLink'],
+}, {
+  name: 'Лесные птицы',
+  classNames: ['headerLink'],
+}, {
+  name: 'Певчие птицы',
+  classNames: ['headerLink'],
+}, {
+  name: 'Хищные птицы',
+  classNames: ['headerLink'],
+}, {
+  name: 'Морские птицы',
+  classNames: ['headerLink'],
+}];
