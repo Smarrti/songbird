2715bdafcb79b8cb74895090193ac5188e3e0cc8
Sergey Bashkin <bashkin2013@mail.ru>
2020-07-31 22:50:25 +0400

    feat: added function for generating questionson game

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 import Header from './Header/Header.jsx';
 import headerLinksArray from './constants/headerLinks';
 import Player from './Player/Player.jsx';
+import questionsArray from './constants/questions';
 
-function App() {
+function createQuestions() {
+  return questionsArray.map((category) => (
+    category[Math.floor(Math.random() * category.length)]
+  ));
+}
+
+export default function App() {
   const [headerLinks] = useState(headerLinksArray);
+  const [questions] = useState(createQuestions());
+  const [currentQuestion] = useState(questions[0]);
   return (
     <>
       <Header headerLinks={headerLinks} />
-      <Player />
+      <Player currentQuestion={currentQuestion} />
     </>
   );
 }
-
-export default App;
