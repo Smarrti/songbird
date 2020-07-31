59538a8233374d3cd6caf06d2db65eb16c8ef1fe
Sergey Bashkin <bashkin2013@mail.ru>
2020-07-31 22:57:31 +0400

    feat: added player in project

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import PropTypes from 'prop-types';
 
 function Player({ currentQuestion }) {
@@ -7,15 +8,15 @@ function Player({ currentQuestion }) {
     <div className="player">
       <img src={currentQuestion.image} alt=""/>
       <div className="player__description">
-        <div>{currentQuestion.bird}</div>
-        <ReactPlayer url={currentQuestion.audioUrl} />
+        <div>{currentQuestion.name}</div>
+        <AudioPlayer showSkipControls="true" src={currentQuestion.audio} />
       </div>
     </div>
   );
 }
 
 Player.propTypes = {
-  currentQuestion: PropTypes.objectOf(PropTypes.string),
+  currentQuestion: PropTypes.objectOf(PropTypes.any),
 };
 
 export default Player;
