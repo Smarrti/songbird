12e580cd022d607d900a938aed813a07d2e1e309
Sergey Bashkin <bashkin2013@mail.ru>
2020-08-01 21:04:39 +0400

    feat: added empty component for right answers

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Answers from '../Answers/Answers.jsx';
+import RightAnswer from '../RightAnswer/RightAnswer.jsx';
 
 function Question({ questions }) {
   return (
     <div className="question">
       <Answers questions={questions} />
+      <RightAnswer />
     </div>
   );
 }
---
+++ src/components/RightAnswer/RightAnswer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RightAnswer() {
+  return (
+    <div className="right-answer"></div>
+  );
+}
+
+export default RightAnswer;
